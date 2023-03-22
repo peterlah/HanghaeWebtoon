e9f7066fb62d65fc2f860bf1fb325336b7726345
peterlah <peterlah@naver.com>
2023-03-22 15:10:45 +0900

    2023-03-22 회원가입 기능 추가

--- README.md
+++ README.md
@@ -13,20 +13,24 @@ source venv/Scripts/activate
 # 비활성화
 deactivate
 
+# UnicodeDecodeError: 'cp949' codec can't decode byte 0xeb in position 16: illegal multibyte sequence
+set PYTHONUTF8=1
+# 설정후 새로운 터미널창 오픈 -> 또는 환경 변수 등록
+
 # pip update
 python.exe -m pip install --upgrade pip
 
 # requests 패키지 -> web에 GET, POST 요청 위해 사용
 pip install requests
 
+# flask 설치 -> 웹 개발 용도
+pip install flask
+
 # bs4 패키지 -> 웹크롤링 패키지
 pip install bs4
 
 # pymongo, dnspython 패키지 -> MongoDB 사용을 위한 패키지
 pip install pymongo dnspython
-
-# json 형식으로 데이터를 변환하는데 도움을 주는 패키지
-pip install jsonify
 
 # 패키지 삭제
 pip uninstall requests
--- app.py
+++ app.py
@@ -1,19 +1,56 @@
+from pymongo import MongoClient
 from flask import Flask, render_template, request, jsonify
-app = Flask(__name__)
+from flask_cors import CORS
 
-@app.route('/')
+app = Flask(__name__)
+CORS(app)
+
+# pyMongo
+# URL 이스케이프 문자 : https://namu.wiki/w/URL%20escape%20code
+client = MongoClient(
+    'mongodb+srv://swlah:%40zbqm1075711@cluster0.g93fmw7.mongodb.net/?retryWrites=true&w=majority')
+db = client.webtoon
+
+# index.html
+@app.route('/', methods=["GET"])
 def home():
     return render_template('index.html')
 
-@app.route('/mypage')
-def mypage():
-    return 'This is My Page!'
+# 회원가입 페이지
+@app.route("/register", methods=["POST"])
+def register_post():
+    user_receive = request.form['user_give']
+    email_receive = request.form['email_give']
+    pw_receive = request.form['pw_give']
+    pw_check_receive = request.form['pw_check_give']
 
-@app.route('/test', methods=['GET'])
-def test_get():
-    title_receive = request.args.get('title_give')
-    print(title_receive)
-    return jsonify({'result':'success', 'msg': '이 요청은 GET!'})
+    if pw_receive != pw_check_receive:
+        return jsonify({'msg': '비밀번호가 일치하지 않습니다.'})
 
-if __name__ == '__main__':  
-    app.run('0.0.0.0',port=5000,debug=True)
+    doc = {
+        'username': user_receive,
+        'mail': email_receive,
+        'password': pw_receive
+    }
+
+    db.user.insert_one(doc)
+
+    return jsonify({'msg':'회원가입 완료!'})
+
+@app.route("/register", methods=["GET"])
+def register_get():
+    return render_template('register.html')
+
+# 로그인 페이지
+@app.route("/login", methods=["POST"])
+def login_post():
+    sample_receive = request.form['sample_give']
+    print(sample_receive)
+    return jsonify({'msg': 'login Page POST 연결 완료!'})
+
+@app.route("/login", methods=["GET"])
+def login_get():
+    return render_template('login.html')
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
