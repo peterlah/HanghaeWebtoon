bc8705009d539f144db19349873f17f14fc4121e
peterlah <peterlah@naver.com>
2023-03-22 21:05:30 +0900

    2023-03-22 로그인 기능 구현

--- app.py
+++ app.py
@@ -7,8 +7,7 @@ CORS(app)
 
 # pyMongo
 # URL 이스케이프 문자 : https://namu.wiki/w/URL%20escape%20code
-client = MongoClient(
-    'mongodb+srv://swlah:%40zbqm1075711@cluster0.g93fmw7.mongodb.net/?retryWrites=true&w=majority')
+client = MongoClient('mongodb+srv://swlah:%40zbqm1075711@cluster0.g93fmw7.mongodb.net/?retryWrites=true&w=majority')
 db = client.webtoon
 
 # index.html
@@ -19,23 +18,24 @@ def home():
 # 회원가입 페이지
 @app.route("/register", methods=["POST"])
 def register_post():
-    user_receive = request.form['user_give']
-    email_receive = request.form['email_give']
-    pw_receive = request.form['pw_give']
-    pw_check_receive = request.form['pw_check_give']
+    user_register_receive = request.form.get('user')
+    email_register_receive = request.form.get('email')
+    pw_register_receive = request.form.get('password')
+    pw_check_receive = request.form.get('re_password')
 
-    if pw_receive != pw_check_receive:
+    if pw_register_receive != pw_check_receive:
         return jsonify({'msg': '비밀번호가 일치하지 않습니다.'})
 
     doc = {
-        'username': user_receive,
-        'mail': email_receive,
-        'password': pw_receive
+        'username': user_register_receive,
+        'mail': email_register_receive,
+        'password': pw_register_receive
     }
 
     db.user.insert_one(doc)
 
     return jsonify({'msg':'회원가입 완료!'})
+
 
 @app.route("/register", methods=["GET"])
 def register_get():
@@ -44,9 +44,21 @@ def register_get():
 # 로그인 페이지
 @app.route("/login", methods=["POST"])
 def login_post():
-    sample_receive = request.form['sample_give']
-    print(sample_receive)
-    return jsonify({'msg': 'login Page POST 연결 완료!'})
+    user_receive = request.form.get('user_give')
+    pw_receive = request.form.get('pw_give')
+    
+    # 디비에서 해당 아이디의 사용자 정보를 조회
+    user = db.user.find_one({'username': user_receive})
+    
+    # 아이디 존재 여부 확인
+    if user is None:
+        return jsonify({'msg': '존재하지 않는 사용자입니다.'})
+    
+    # 비밀번호 일치 여부 확인
+    if user.get('password') != pw_receive:
+        return jsonify({'msg': '비밀번호가 일치하지 않습니다.'})
+    else:
+    	return jsonify({'msg': 'login 성공!'})
 
 @app.route("/login", methods=["GET"])
 def login_get():
